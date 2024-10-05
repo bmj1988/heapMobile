8c2a4493a7bb0bb43c93d3e46570ad2cb3045350
Brian Johnson <126127671+bmj1988@users.noreply.github.com>
2024-10-05 16:35:56 -0400

    Changed listing tags to conform to format

--- components/ListingTag.jsx
+++ components/ListingTag.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 const ListingTag = ({ tag }) => {
     return (
-        <View className="rounded-3xl border-gray-100 border-solid border-[1px] bg-black-100 w-20 py-1 justify-center items-center m-1">
-            <Text className="color-mint">{tag.name}</Text>
+        <View className="rounded-3xl border-gray-100 border-solid border-[1px] bg-black-100 h-7 w-20 py-1 justify-center items-center m-1 overflow-y-hidden overflow-x-scroll">
+            <Text className="color-mint text-center" numberOfLines={1}>{tag.name}</Text>
         </View>
     )
 }
