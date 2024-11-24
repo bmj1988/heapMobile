c5b64299ce2dae0f40817db2fa62b716067c52df
Brian Johnson <126127671+bmj1988@users.noreply.github.com>
2024-11-23 20:29:50 -0500

    added total listings information to profile page

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -1,11 +1,21 @@
-import { View, Text } from 'react-native'
+import { View, Text, SafeAreaView, Image } from 'react-native'
 import React from 'react'
 
 const Profile = () => {
+  const { user, page } = useGlobalContext()
+  // const userData = useAppwrite(fetchReviewData(user.$id))
   return (
-    <View>
-      <Text>Profile</Text>
-    </View>
+    <SafeAreaView>
+      <View style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <Image className="w-[60%] rounded-2xl" source={{ uri: user.avatar }} />
+      </View>
+      <View>
+        <Text>{userData.listings.length}</Text>
+        {/* <Text>{userData.ratingAverage} {`( ${userData.numRatings} )`}*/}
+        {/* user can set area, or more than one area (max 2) for the day {user.area} */}
+        {/* user.profile */}
+      </View>
+    </SafeAreaView>
   )
 }
 
