e9c79ed8afab6b5a28a3d143b353c92527f90c1d
Brian Johnson <126127671+bmj1988@users.noreply.github.com>
2025-01-04 02:06:39 -0500

    Delete functionality added for edit price card

--- components/AddCardFooter.jsx
+++ components/AddCardFooter.jsx
@@ -2,7 +2,7 @@ import { View, Text, TextInput, Pressable, Modal, FlatList } from 'react-native'
 import React, { useEffect, useRef, useState } from 'react'
 import TagSelectionModal from './Modals/TagSelectionModal'
 import { useGlobalContext } from '@/context/GlobalProvider'
-import { createNewCard, getAllTags, updateCard } from '../lib/appwrite'
+import { createNewCard, deleteCard, getAllTags, updateCard } from '../lib/appwrite'
 import useAppwrite from '../lib/useAppwrite'
 import { FontAwesome } from '@expo/vector-icons'
 import CustomButton from './CustomButton'
@@ -17,7 +17,7 @@ const AddCardFooter = ({ setPrices, prices, edit = null, formOpen = false, setCa
     const [isLoading, setIsLoading] = useState(false)
     const defaultFormState = {
         user: user.$id,
-        material: "Material",
+        material: "",
         price: 0.00,
         tags: null,
         type: "lb",
@@ -44,34 +44,57 @@ const AddCardFooter = ({ setPrices, prices, edit = null, formOpen = false, setCa
 
     console.log('FORM', form)
 
-    const submit = async () => {
+    const wipeForm = (newPrices) => {
+        setPrices(newPrices)
+        setFormDisplay(false)
+        setForm(defaultFormState)
+        setCard(null)
+        return
+    }
+
+    const submitEdit = async () => {
         setIsLoading(true)
         try {
-            if (edit) {
-                let update = await updateCard(form)
-                let otherPrices = prices.filter((card) => card.$id !== form.$id)
-                setPrices([...otherPrices, update])
-                setFormDisplay(false)
-                setCard(null)
-                setForm(defaultFormState)
-                console.log("Success")
-                // going to change from this appwrite logic to a function
-                // that UPSERTS so this should not be an issue
-                // let update = await updateCard(form)
-                // let otherPrices = prices.filter((card) => card.$id !== form.$id)
-                // setPrices([...otherPrices, update])
-                // setFormDisplay(false)
-                // setForm(defaultFormState)
-            }
-            let newCard = await createNewCard(form)
-            if (!newCard) throw new Error("Could not create new card")
-            setPrices([...prices, newCard])
-            setFormDisplay(false)
-            setForm(defaultFormState)
+            let update = await updateCard(form)
+            let otherPrices = prices.filter((card) => card.$id !== form.$id)
+            wipeForm([...otherPrices, update])
+            console.log("Submit Edit - Success")
         }
         catch (e) {
             console.error(e)
             throw new Error(e)
+        }
+        finally {
+            setIsLoading(false)
+        }
+    }
+
+    const submitCreate = async () => {
+        setIsLoading(true)
+        try {
+            let newCard = await createNewCard(form)
+            if (!newCard.$id) throw new Error("Could not create new card")
+            wipeForm([...prices, newCard])
+        }
+        catch (e) {
+            console.error(e)
+            throw new Error(e)
+        }
+        finally {
+            setIsLoading(false)
+        }
+    }
+
+    const submitDelete = async () => {
+        setIsLoading(true)
+        try {
+            await deleteCard(edit.$id)
+            const otherPrices = prices.filter((price) => price.$id !== edit.$id)
+            wipeForm(otherPrices)
+
+        }
+        catch (e) {
+            console.error(e)
         }
         finally {
             setIsLoading(false)
@@ -111,61 +134,64 @@ const AddCardFooter = ({ setPrices, prices, edit = null, formOpen = false, setCa
                 <FontAwesome name={"plus"} color={"#50bf88"} size={22} />
             </Pressable>
             {formDisplay &&
-                <View className={`h-fit w-[full] bg-primary p-1 flex-row items-center justify-between pr-3 pl-3`}>
-                    <View className={`items-center justify-start w-[35%]`}>
-                        <Text className="color-gray-100 text-rsthin">{"Material"}</Text>
-                        {edit &&
-                            <Text className="text-xl color-mint font-rsbold">
-                                {edit.material}
-                            </Text>}
-                        {!edit &&
-                            <View style={{ display: 'flex', flexDirection: 'row' }}>
-                                <Pressable
-                                    onPress={() => edit ? null : setTagModalVisible(true)}
-                                    className="justify-start items-center">
-                                    <FontAwesome name={"tag"} color={"#50bf88"} size={25} />
-                                    <Text className="color-gray-100 text-rsthin">Add a tag</Text>
-                                </Pressable>
-                                <TextInput
-                                    className="bg-black-200 color-mint text-rsregular w-[80px] h-[33px] p-1 rounded-md m-1"
-                                    onChangeText={(text) => setForm({ ...form, material: text })}
-                                    value={form.material}
-                                // autoFocus={true}
-                                />
-                            </View>}
-
-                    </View>
-                    <View className="h-full items-center justify-start w-[10%]">
-                        <Text className="color-gray-100 text-rsthin">{"Price"}</Text>
-                        <TextInput
-                            value={parseFloat(form.price).toFixed(2)}
-                            className="bg-black-200 color-mint text-rsregular w-[50px] h-[33px] p-1 rounded-md m-1"
-                            inputMode='decimal'
-                            onChangeText={(text) => setForm({ ...form, price: parseFloat(text) })} />
-                    </View>
-                    <View className="items-center justify-center w-[20%]">
-                        <Text className="color-gray-100 text-rsthin">{"per"}</Text>
-                        <Pressable onPress={() => {
-                            console.log('click')
-                            setDropdownVisible(true)
-                        }
-                        }>
-                            <Text className={`font-rsbold color-mint text-xl bg-black-200 p-1 rounded-md m-1`}>{form.type}</Text>
-                        </Pressable>
-                        {dropdownVisible &&
-                            <View style={{ position: 'absolute', bottom: 1 }} className="p-1 rounded-md m-1 bg-black-200">
-                                {units.map((unit) => (
-                                    <Pressable key={unit} onPress={() => {
-                                        setForm({ ...form, type: unit })
-                                        setDropdownVisible(false)
-                                    }}>
-                                        <Text className={`font-rsbold color-mint text-xl bg-black-200 p-1`}>{form.type}</Text>
+                <View>
+                    <View className={`h-fit w-[full] bg-primary p-1 flex-row items-center justify-between pr-3 pl-3`}>
+                        <View className={`items-center justify-start w-[35%]`}>
+                            <Text className="color-gray-100 text-rsthin">{"Material"}</Text>
+                            {edit &&
+                                <Text className="text-xl color-mint font-rsbold">
+                                    {edit.material}
+                                </Text>}
+                            {!edit &&
+                                <View style={{ display: 'flex', flexDirection: 'row' }}>
+                                    <Pressable
+                                        onPress={() => edit ? null : setTagModalVisible(true)}
+                                        className="justify-start items-center">
+                                        <FontAwesome name={"tag"} color={"#50bf88"} size={25} />
+                                        <Text className="color-gray-100 text-rsthin">Add a tag</Text>
                                     </Pressable>
-                                ))}
-                            </View>
-                        }
-                    </View>
-                    {/* <View className="items-center justify-center w-[25%]">
+                                    <TextInput
+                                        className="bg-black-200 color-mint text-rsregular w-[80px] h-[33px] p-1 rounded-md m-1"
+                                        onChangeText={(text) => setForm({ ...form, material: text })}
+                                        value={form.material}
+                                        placeholder='Material?'
+                                        placeholderTextColor={"#CDCDE0"}
+                                        autoFocus={true}
+                                    />
+                                </View>}
+
+                        </View>
+                        <View className="h-full items-center justify-start w-[10%]">
+                            <Text className="color-gray-100 text-rsthin">{"Price"}</Text>
+                            <TextInput
+                                value={parseFloat(form.price).toFixed(2)}
+                                className="bg-black-200 color-mint text-rsregular w-[50px] h-[33px] p-1 rounded-md m-1"
+                                inputMode='decimal'
+                                onChangeText={(text) => setForm({ ...form, price: parseFloat(text) })} />
+                        </View>
+                        <View className="items-center justify-center w-[20%]">
+                            <Text className="color-gray-100 text-rsthin">{"per"}</Text>
+                            <Pressable onPress={() => {
+                                console.log('click')
+                                setDropdownVisible(true)
+                            }
+                            }>
+                                <Text className={`font-rsbold color-mint text-xl bg-black-200 p-1 rounded-md m-1`}>{form.type}</Text>
+                            </Pressable>
+                            {dropdownVisible &&
+                                <View style={{ position: 'absolute', bottom: 1 }} className="p-1 rounded-md m-1 bg-black-200">
+                                    {units.map((unit) => (
+                                        <Pressable key={unit} onPress={() => {
+                                            setForm({ ...form, type: unit })
+                                            setDropdownVisible(false)
+                                        }}>
+                                            <Text className={`font-rsbold color-mint text-xl bg-black-200 p-1`}>{form.type}</Text>
+                                        </Pressable>
+                                    ))}
+                                </View>
+                            }
+                        </View>
+                        {/* <View className="items-center justify-center w-[25%]">
                         <Text className="color-gray-100 text-rsthin">{"Minimum\n(optional)"}</Text>
                         <TextInput
                             inputMode='numeric'
@@ -174,8 +200,13 @@ const AddCardFooter = ({ setPrices, prices, edit = null, formOpen = false, setCa
                                 if (min > 0) setForm({ ...form, minimum: min })
                             }} />
                     </View> */}
-                    <CustomButton title={"ADD"} handlePress={() => submit()} isLoading={isLoading} containerStyles={'p-2 h-[fit-content]'} />
-                    <TagSelectionModal visible={tagModalVisible} closeModal={() => setTagModalVisible(false)} form={form} setForm={setForm} tagList={tags} currentTags={form.tags} card={true} />
+                        <CustomButton title={`${edit ? "EDIT" : "ADD"}`} handlePress={edit ? () => submitEdit() : () => submitCreate()} isLoading={isLoading} containerStyles={'p-2 h-[fit-content]'} />
+                        <TagSelectionModal visible={tagModalVisible} closeModal={() => setTagModalVisible(false)} form={form} setForm={setForm} tagList={tags} currentTags={form.tags} card={true} />
+                    </View>
+                    {
+                        edit &&
+                        <CustomButton title="Delete Price Card" handlePress={() => submitDelete()} containerStyles={"bg-carmine m-2"} isLoading={isLoading} />
+                    }
 
                 </View>
             }
