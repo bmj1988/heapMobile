845cbc1a5c0bae03c95de85a9470dd2bafc0008a
Brian Johnson <126127671+bmj1988@users.noreply.github.com>
2025-02-01 17:17:27 -0500

    Bid accept works, updates bid to accepted, updates listing to closed, updates bid acceptedOn to correct date

--- components/listingComponents/BidDisplay.jsx
+++ components/listingComponents/BidDisplay.jsx
@@ -1,22 +1,31 @@
 import { View, Text, Pressable } from 'react-native'
 import { Entypo } from '@expo/vector-icons'
+import { acceptBid } from '../../lib/appwrite'
+import CustomButton from '../CustomButton'
+import { useState } from 'react'
+
 const BidDisplay = ({ bid }) => {
+
+    const [isLoading, setIsLoading] = useState(false)
+    const handleAcceptBid = async () => {
+        setIsLoading(true)
+        await acceptBid(bid.$id)
+        setIsLoading(false)
+    }
+
+
     return (
-        <View className="bg-black-200 rounded-2xl m-1 flex-row items-center justify-around">
+        <View className="bg-black-200 rounded-2xl m-2 flex-row items-center justify-around">
             <View className="m-1 items-center justify-center">
                 <Text className="color-gray-100 text-rsthin">Offer</Text>
+
                 <Text className="text-2xl font-rssemibold color-mint">{`$${bid.offer}`}</Text>
             </View>
             <View className="m-1 items-center justify-center">
                 <Text className="color-gray-100 text-rsthin">User</Text>
                 <Text className="text-xl font-rsthin color-mint">{bid.buyer.username}</Text>
             </View>
-            <Pressable onPress={() => console.log("accept bid", bid.$id)}>
-                <Entypo
-                    name={"check"}
-                    color={"#50bf88"}
-                    size={30} />
-            </Pressable>
+            <CustomButton title="Accept" handlePress={handleAcceptBid} containerStyles="min-h-[40px] p-2" isLoading={isLoading} />
         </View>
     )
 }
